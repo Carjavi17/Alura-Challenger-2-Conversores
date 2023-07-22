88a169bb6ec4fd52cb4fbdfa80286bedcc942a9b
Carlos Guerra <carlosjguerrab17@gmail.com>
2023-07-22 01:38:53 -0300

    Terminado Conversor de Moneda

--- src/Conversor/Main.java
+++ src/Conversor/Main.java
@@ -1,5 +1,7 @@
 package Conversor;
 
+import java.text.DecimalFormat;
+
 import javax.swing.JOptionPane;
 
 public class Main {
@@ -23,16 +25,38 @@ public class Main {
 	    }
 
 	    public static void convertirMoneda() {
-	    	 String montoEnPesos = JOptionPane.showInputDialog(null, "Ingrese el monto en pesos Argentinos");
-
+	    	 String montoEnPesos = JOptionPane.showInputDialog(null, "Ingrese en pesos el monto que desea convertir");
+	    	 double montoIngresado = Double.parseDouble(montoEnPesos);
+	    	 
 	         if (montoEnPesos == null || montoEnPesos.isEmpty()) {
 	             JOptionPane.showMessageDialog(null, "No se ingresó un valor válido.", "Error", JOptionPane.ERROR_MESSAGE);
 	             System.exit(0);
 	         }
-	         double montoIngresado = Double.parseDouble(montoEnPesos);
-	         double tasaDeCambio = 520;
-	         double montoEnDolares = montoIngresado / tasaDeCambio;
-	         JOptionPane.showMessageDialog(null, montoIngresado+ " En pesos Argentinos corresponde a " + montoEnDolares + " en dolares Americanos.");
+	         String opciones[] = {"De pesos a Dolar", "De pesos a Euros", "De pesos a Reales"};
+	         String op = (String) JOptionPane.showInputDialog(null, "Elija una Opción", "Menu", JOptionPane.DEFAULT_OPTION, null, opciones, opciones[0]);
+	         DecimalFormat decimalFormat = new DecimalFormat("#.##");
+	         
+	         if (op != null) {
+		            if (op.equals("De pesos a Dolar")) {
+		   	         double tasaDeCambio = 520;
+			         double montoEnDolares = montoIngresado / tasaDeCambio;
+			         String resultadoFormateado = decimalFormat.format(montoEnDolares);
+			         JOptionPane.showMessageDialog(null, montoIngresado+ " En pesos Argentinos corresponde a " + resultadoFormateado + " en dolares Americanos.");
+		            } else if (op.equals("De pesos a Euros")) {
+		   	         double tasaDeCambio = 575;
+			         double montoEnEuros = montoIngresado / tasaDeCambio;
+			         String resultadoFormateado = decimalFormat.format(montoEnEuros);
+			         JOptionPane.showMessageDialog(null, montoIngresado+ " En pesos Argentinos corresponde a " + resultadoFormateado + " en Euros.");
+		            } else if (op.equals("De pesos a Reales")) {
+		   	         double tasaDeCambio = 56;
+			         double montoEnReales = montoIngresado / tasaDeCambio;
+			         String resultadoFormateado = decimalFormat.format(montoEnReales);
+			         JOptionPane.showMessageDialog(null, montoIngresado+ " En pesos Argentinos corresponde a " + resultadoFormateado + " en Reales.");
+		            } else {
+		                JOptionPane.showMessageDialog(null, "Opción inválida.", "Error", JOptionPane.ERROR_MESSAGE);
+		            }
+		        }    
+
 	    }
 
 	    public static void convertirTemperatura() {
@@ -50,6 +74,16 @@ public class Main {
 	    }
 
 	    public static void convertirLongitud() {
+	    	String longitudStr = JOptionPane.showInputDialog(null, "Ingrese la longitud en la unidad original (metros):");
+
+	        if (longitudStr == null || longitudStr.isEmpty()) {
+	            JOptionPane.showMessageDialog(null, "No se ingresó un valor válido.", "Error", JOptionPane.ERROR_MESSAGE);
+	            System.exit(0);
+	        }
+
+	        double longitudMetros = Double.parseDouble(longitudStr);
+	        double longitudKilometros = longitudMetros / 1000;
+	        JOptionPane.showMessageDialog(null, longitudMetros + " metros equivalen a " + longitudKilometros + " kilómetros.");
 
 	    }
 
